2e4bad524ea7fa9b3439ca71102feb6dbb7b354e
mkjubran <mjubran@birzeit.edu>
2026-05-09 11:49:43 +0300

    ver0.0

--- train_stgcn_server.py
+++ train_stgcn_server.py
@@ -1246,7 +1246,7 @@ def lr_lambda(epoch):
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimiser, mode='min', factor=0.5,
-    patience=10, min_lr=1e-6, verbose=True
+    patience=25, min_lr=1e-6, verbose=True
 )
 early_stop = EarlyStopping(patience=PATIENCE, min_delta=MIN_DELTA)
 
